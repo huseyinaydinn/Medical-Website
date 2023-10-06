046afb73ccde067c5ab1cc493a0644a1a9e0e1b7
Hüseyin Aydın <hsynaydino@gmail.com>
2023-10-06 23:04:14 +0300

    services/team/about section build

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,3 +17,38 @@ window.addEventListener("scroll", () => {
     navbarScroll.classList.remove("scrolled");
   }
 });
+
+// Hamburger menü açıldığı zaman menüye bir class eklemek için açık kapalı olma durumunu izledim. Menü açıkken sayfayı kaplasın ve scroll çıksın istiyorum.
+const navbarToggler = document.querySelector(".navbar-toggler");
+const navbarMenu = document.querySelector(".navbar");
+
+navbarToggler.addEventListener("click", () => {
+  if (navbarToggler.classList.toString().includes("collapse")) {
+    navbarMenu.classList.remove("open");
+  } else {
+    navbarMenu.classList.add("open");
+  }
+});
+
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 40,
+    },
+    992: {
+      slidesPerView: 3,
+      spaceBetween: 50,
+    },
+  },
+});
